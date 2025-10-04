f602351e30e96e18832e72feca8a72400971ec22
LuisMiguel-Valera <luismiguelvalera@gmail.com>
2025-10-04 18:42:43 -0500

    Update app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Cargar los datos del archivo CSV
+car_data = pd.read_csv('../vehicles_us.csv')
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
